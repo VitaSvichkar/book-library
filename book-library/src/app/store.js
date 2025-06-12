b18a522a5431b407879368eee8d5542b33e979df
Viktoriia Svichkar <vitasvichkar@gmail.com>
2025-06-12 20:44:31 +0300

    refactor: remove saving 'books' slice to localStorage

--- book-library/src/app/store.js
+++ book-library/src/app/store.js
@@ -16,7 +16,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['myBooks', 'books'],
+  whitelist: ['myBooks'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
